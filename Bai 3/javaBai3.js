e79064e8f03f9d21c2e98a62dd9c3949839db142
Hung-qwerty <viethun.mc2023@gmail.com>
2025-04-09 09:12:12 +0700

    Update javaBai3.js

--- Bai 3/javaBai3.js
+++ Bai 3/javaBai3.js
@@ -1,8 +1,21 @@
 const randomNumber = Math.floor(Math.random() * 10) + 1;
+let userGuess = null;
+while (userGuess !== randomNumber) {
+  const input = prompt("Hãy đoán một số từ 1 đến 10:");
+  
+  if (input === null) {
+    alert("Bạn muốn thoát chứ?");
+    break;
+  }
+  userGuess = parseInt(input);
 
-let userGuess;
-do {
-  userGuess = parseInt(prompt("Hãy đoán một số từ 1 đến 10:"));
-} while (userGuess !== randomNumber);
+  if (isNaN(userGuess) || userGuess < 1 || userGuess > 10) {
+    alert("Vui lòng nhập một số hợp lệ từ 1 đến 10.");
+  } else if (userGuess !== randomNumber) {
+    alert("Sai rồi thử lại.");
+  }
+}
 
-alert("Chúc mừng! Bạn đã đoán đúng số " + randomNumber);
+if (userGuess === randomNumber) {
+  alert("Bạn đã đoán đúng số! " + randomNumber);
+}
